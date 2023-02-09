be984f4ea7052a2d4a5ee6468eacffb3f3d32d2b
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2023-02-09 18:03:22 +0700

    handle show data to ui

--- script.js
+++ script.js
@@ -4,23 +4,31 @@ const $ = document.querySelector.bind(document);
 
 const locationInput = $(".location__Input");
 const checkBtn = $(".checkBtn");
-const weatherWrapper = $(".weather__wrapper");
-const weatherTitle = $(".weather__title");
+const contentWrapper = $(".contentWrapper");
+const weather__current = $(".weather__current");
+const toast = $("#toast");
+
+
+// const weatherWrapper = $(".weather__wrapper");
+// const weatherTitle = $(".weather__title");
+
+
+
+//variable to save element
+let weatherCurrentValue;
+let locationInputValue;
 
 
 //
-let locationInputValue;
+let defaultLocationValue = "VietNam";
 
-//variable to save data from api
-let dataCurrent, dataLocation, dataForecast;
 
-//variable to save element
-let dataBody;
-let weatherTitleValue;
 
 
 checkBtn.addEventListener("click", handleCheckWeather);
-
+window.onload = function (e) {
+    getData(defaultLocationValue);
+}
 
 //function
 
@@ -29,88 +37,131 @@ checkBtn.addEventListener("click", handleCheckWeather);
 function handleCheckWeather() 
 {   
     locationInputValue = locationInput.value;
-    validateInput(locationInput);
+    validateInput(locationInputValue);
     
 }
 
 //function to get data from api
-function getData() 
+function getData(value) 
 {
-    const response = fetch(`http://api.weatherapi.com/v1/current.json?key=4acb30f8960e480c96131445230802&q=${locationValue}&aqi=no`);
+    const response = fetch(`http://api.weatherapi.com/v1/current.json?key=4acb30f8960e480c96131445230802&q=${value}&aqi=no`);
     response.then(res => res.json())
-    .then((data) => isExistLocation(data));
-}
-
-//function to show location 
-function showLocation (dataLocation)
-{
-    weatherTitleValue = ` 
-                            <span>${dataLocation.name}</span>
-                            <span>Weather Forecast</span>
-                        `;
-    weatherTitle.innerHTML += weatherTitleValue;
-
-}
-
-//function to check whether location exist or not
-function isExistLocation(data)
-{
-    dataLocation = data.location;
-    if (dataLocation != null) 
+    .then((data) =>
     {
-        showLocation(dataLocation);
-        return true;
+        if(data.error)
+        {
+            showNotExistToast(value);
+        }
+       else
+       {
+            showWeatherCurrent(data);
+       }
+    })
+    .catch((error) => 
+    {
+        console.log('error', error);
+    })
+}
+
+
+function showWeatherCurrent(data) 
+{
+    weatherCurrentValue =
+        `
+        <div class="weather__current">
+            <span class="tempC">${data.current.temp_c} &deg;</span>
+           <div class="weather_info">
+                <div class="location">
+                    <span class="location__name">
+                        ${data.location.name}
+                    </span> 
+                    <span class="location___time">
+                        ${data.location.localtime}
+                    </span>
+                </div>
+                <div class="condition">
+                    <img class="condition__icon" src="${data.current.condition.icon}">
+                    <span class="condition__status">${data.current.condition.text}</span>
+                </div>
+           </div>
+        </div> 
+        `;
+    contentWrapper.innerHTML = weatherCurrentValue;
+}
+
+function handleToast({message}) {
+
+
+    const toastValue = document.createElement('div');
+    //Auto remove toast
+    const autoRemoveID = setTimeout(function () {
+        toast.removeChild(toastValue);
+    },4000);
+
+    toastValue.onclick = function (e) {
+        if (e.target.closest('.toast__close')) {
+            toast.removeChild(toastValue);
+            clearTimeout(autoRemoveID); //để settimeout kh chạy nữa
+        }
     }
-    return false;
+    toastValue.classList.add('toastWrapper');
+    toastValue.innerHTML = `
+                   
+                        <div class="toast__icon">
+                             <i class="fas fa-exclamation-circle"></i>
+                        </div>
+                        <div class="toast__body">
+                            <h3 class="toast__title">Error</h3>
+                            <p class="toast__msg">${message}</p>
+                        </div>
+                        <div class="toast__close">
+                            <i class="fas fa-times-circle"></i>
+                        </div>
+                 
+                `;
+
+    toast.appendChild(toastValue);
+}
+function showNotExistToast(value) {
+    handleToast(
+        {
+            message:    `Not exist ${value}. Please try another location again.`,
+        }
+    );
+}
+function showEmptyToast() {
+        handleToast (
+            {
+                message:   `This input is empty`
+            }
+        )
 }
 
 //function to check value is empty or not
 function checkEmptyValue (value) 
 {
 
-    if (value === "") {
+    if (value === "") 
+    {
+            return true;
+    }
+    else
+    {
         return false;
-      } else {
-        return true;
-      }
-
-    // if(value === "") 
-    // {
-    //     return true;
-    // }
-    // else 
-    // {
-    //     return false;
-    // }
+    }
+      
 }
 
 //function to validate input
-function validateInput() 
+function validateInput(value) 
 {
-    if(checkEmptyValue) 
+    if(!checkEmptyValue(value)) 
     {
-        console.log("Not empty");
-       if (!isExistLocation) 
-       {
-        console.log("Not exist location");
-       }
+        getData(value)
     }
     else 
     {
-        console.log("Empty value");
+       showEmptyToast();
     }
 }
 
-
-
-// function showTimeZone(data) {
-//     dataCurrent = data.current;
-   
-//     dataForecast = data.dataForecast;
-
-//     weatherTitleValue = ` 
-//                         <span>${dataLocation.name}</span>
-//                         <span>Weather Forecast</span>
-//                     `;
-//     weatherTitle.innerHTML += weatherTitleValue;
-// }
