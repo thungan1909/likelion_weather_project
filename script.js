9eeba4c30b93008e0681f22a6c1cf2915ede483c
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2023-02-10 16:59:49 +0700

    update ui

--- script.js
+++ script.js
@@ -3,34 +3,62 @@ const $ = document.querySelector.bind(document);
 
 
 const locationInput = $(".location__Input");
-const checkBtn = $(".checkBtn");
+// const checkBtn = $(".checkBtn");
+
+const formInput = document.getElementById("inputForm");
+
 const contentWrapper = $(".contentWrapper");
 const weather__current = $(".weather__current");
 const toast = $("#toast");
 
 
-// const weatherWrapper = $(".weather__wrapper");
-// const weatherTitle = $(".weather__title");
-
-
-
 //variable to save element
 let weatherCurrentValue;
 let locationInputValue;
+let weatherDetailInDayArray;
+let tempCInDayArray;
 
 
 //
 let defaultLocationValue = "VietNam";
 
+// var xValues = [50,60,70,80,90,100,110,120,130,140,150];
+// var yValues = [7,8,8,9,9,9,10,11,14,14,15];
 
+// new Chart("temperChart", {
+//   type: "line",
+//   data: {
+//     labels: xValues,
+//     datasets: [{
+//       fill: false,
+//       lineTension: 0,
+//       backgroundColor: "red",
+//       borderColor: "red",
+//       data: yValues
+//     }]
+//   },
+//   options: {
+//     legend: {display: false},
+//     scales: {
+//       yAxes: [{ticks: {min: 6, max:16}}],
+//     }
+//   }
+// });
+// checkBtn.addEventListener("click", handleCheckWeather);
+function handleSubmit (e) 
+{
+    e.preventDefault();
+    console.log(locationInput.value);
+}
+formInput.onsubmit(e) = handleSubmit(e);
 
-
-checkBtn.addEventListener("click", handleCheckWeather);
 window.onload = function (e) {
-    getData(defaultLocationValue);
+    // getData(defaultLocationValue);
+
 }
 
 //function
+
 
 
 //function to handle check weather
@@ -44,7 +72,7 @@ function handleCheckWeather()
 //function to get data from api
 function getData(value) 
 {
-    const response = fetch(`http://api.weatherapi.com/v1/current.json?key=4acb30f8960e480c96131445230802&q=${value}&aqi=no`);
+    const response = fetch(`http://api.weatherapi.com/v1/forecast.json?key=4acb30f8960e480c96131445230802&q=${value}&days=10&aqi=yes&alerts=no`);
     response.then(res => res.json())
     .then((data) =>
     {
@@ -54,7 +82,10 @@ function getData(value)
         }
        else
        {
-            showWeatherCurrent(data);
+           // console.log(data);
+           // getWeatherByHour(data);
+            
+            //showWeatherCurrent(data);
        }
     })
     .catch((error) => 
@@ -63,7 +94,19 @@ function getData(value)
     })
 }
 
-
+function getWeatherByHour(data)
+{
+    weatherDetailInDayArray = data.forecast.forecastday[0];
+    for(let i = 0; i <= weatherDetailInDayArray.hour.length; i++)
+    {
+        console.log(weatherDetailInDayArray.hour[i].temp_c);
+    }
+    // console.log(weatherDetailInDayArray);
+    // for (let i = 0; i <= arr.length; i++) 
+    // {
+    //     console.log(arr[i].temp_c);
+    // }
+}
 function showWeatherCurrent(data) 
 {
     weatherCurrentValue =
